79b0bf060b41327be03603b3f23cfcbc1526959d
Lolo280374 <56828143+Lolo280374@users.noreply.github.com>
2025-08-31 18:06:19 +0200

    try

---
+++ index.tsx
@@ -0,0 +1,111 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2024 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+import definePlugin, { OptionType } from '@utils/types';
+import { showNotification } from "@api/Notifications";
+import { definePluginSettings } from '@api/Settings';
+import { ModalContent, ModalFooter, ModalHeader, ModalRoot, openModal } from "@utils/modal";
+import { Button, Forms, TextArea } from "@webpack/common";
+
+let lastHeartbeatAt = 0;
+
+const settings = definePluginSettings({
+    apiKey: {
+        type: OptionType.STRING,
+        description: 'API Key for hackatime',
+        default: 'AAAAAAAAAAAAA',
+        isValid: (e: string) => {
+            if (e === "AAAAAAAAAAAAA") return "Invalid Key: Please change the default API Key";
+            return true;
+        },
+    },
+    debug: {
+        type: OptionType.BOOLEAN,
+        description: 'Enable debug mode',
+        default: false,
+    },
+    machineName: {
+        type: OptionType.STRING,
+        description: 'Hostname',
+        default: 'Vencord',
+    },
+    projectName: {
+        type: OptionType.STRING,
+        description: "Project Name",
+        default: "Discord",
+    },
+});
+
+function enoughTimePassed() {
+    return lastHeartbeatAt + 120000 < Date.now();
+}
+
+async function sendHeartbeat(time) {
+    const key = settings.store.apiKey;
+    if (!key || key === 'AAAAAAAAAAAAA') {
+        showNotification({
+            title: "Hackatime",
+            body: "Don't forget to input your Hackatime API key within settings.",
+            color: "var(--red-360)",
+        });
+
+        return;
+    }
+    if (settings.store.debug) {
+        console.log('Sending heartbeat to Hackatime API.');
+    }
+
+    const url = 'https://hackatime.hackclub.com/api/hackatime/v1';
+    const body = JSON.stringify({
+        time: time / 1000,
+        entity: 'Discord',
+        type: 'app',
+        project: settings.store.projectName ?? "Discord",
+        plugin: 'vencord/version discord-hackatime/v0.0.1',
+    });
+    const headers = {
+        Authorization: `Basic ${key}`,
+        'Content-Type': 'application/json',
+        'Content-Length': new TextEncoder().encode(body).length.toString(),
+    };
+    const machine = settings.store.machineName;
+    if (machine) headers['X-Machine-Name'] = machine;
+    const response = await fetch(url, {
+        method: 'POST',
+        body: body,
+        headers: headers,
+    });
+    const data = await response.text();
+    if (response.status < 200 || response.status >= 300) console.warn(`Hackatime API Error ${response.status}: ${data}`);
+}
+
+async function handleAction() {
+    const time = Date.now();
+    if (!enoughTimePassed()) return;
+    lastHeartbeatAt = time;
+    await sendHeartbeat(time);
+}
+
+export default definePlugin({
+    name: 'hackatime',
+    description: 'Track your Discord stats within Hackatime',
+    authors: [
+        {
+            id: 547101350450692142,
+            name: 'lolo.zip',
+        },
+    ],
+    settings,
+    start() {
+        console.log('Initializing Hackatime plugin');
+        this.handler = handleAction.bind(this);
+        document.addEventListener('click', this.handler);
+    },
+    stop() {
+        console.log('Unloading Hackatime plugin');
+        document.removeEventListener('click', this.handler);
+    },
+});
