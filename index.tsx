1246949915507ed76ed6b89571372a6d0cce9bdd
Lolo280374 <56828143+Lolo280374@users.noreply.github.com>
2025-09-03 14:22:55 +0200

    why tf did github merge so bad

--- index.tsx
+++ index.tsx
@@ -10,30 +10,17 @@ import { definePluginSettings } from "@api/Settings";
 const settings = definePluginSettings({
     apiKey: {
         type: OptionType.STRING,
-<<<<<<< HEAD
         description: "Hackatime API key",
         default: "CHANGEME",
     },
     debug: {
         type: OptionType.BOOLEAN,
         description: "Show more debugging info",
-=======
-        description: "API Key for Wakatime",
-        default: "",
-    },
-    debug: {
-        type: OptionType.BOOLEAN,
-        description: "Enable debug mode",
->>>>>>> 3fcdca8725863981d01c73677c1114ada0aff130
         default: false,
     },
     machineName: {
         type: OptionType.STRING,
-<<<<<<< HEAD
         description: "Hostname",
-=======
-        description: "Machine name",
->>>>>>> 3fcdca8725863981d01c73677c1114ada0aff130
         default: "Vencord User",
     },
     projectName: {
@@ -49,11 +36,7 @@ async function sendHeartbeat() {
 
     if (!apiKey) return;
 
-<<<<<<< HEAD
     if (debug) console.log("Sending heartbeat to Hackatime API.");
-=======
-    if (debug) console.log("Sending heartbeat to WakaTime API.");
->>>>>>> 3fcdca8725863981d01c73677c1114ada0aff130
 
     const url = "https://hackatime.hackclub.com/api/hackatime/v1";
     const body = JSON.stringify({
@@ -61,11 +44,7 @@ async function sendHeartbeat() {
         entity: "Discord",
         type: "app",
         project: projectName ?? "Discord",
-<<<<<<< HEAD
         plugin: "vencord/version discord-hackatime/v0.1",
-=======
-        plugin: "vencord/version discord-wakatime/v0.0.1",
->>>>>>> 3fcdca8725863981d01c73677c1114ada0aff130
     });
 
     const headers = {
@@ -83,7 +62,6 @@ async function sendHeartbeat() {
 
     const data = await res.text();
 
-<<<<<<< HEAD
     if (res.status !== 200) console.warn(`Hackatime API Error ${res.status}: ${data}`);
 }
 
@@ -92,17 +70,6 @@ export default definePlugin({
     description: "Track your Discord usage within Hackatime!",
     authors: [
         { name: "lolo.zip", id: 547101350450692142 }
-=======
-    if (res.status !== 200) console.warn(`WakaTime API Error ${res.status}: ${data}`);
-}
-
-export default definePlugin({
-    name: "Wakatime",
-    description: "Fully automatic code stats via Wakatime",
-    authors: [
-        { name: "Neon", id: 566766267046821888n },
-        { name: "thororen", id: 848339671629299742n }
->>>>>>> 3fcdca8725863981d01c73677c1114ada0aff130
     ],
     settings,
     sendHeartbeat,
